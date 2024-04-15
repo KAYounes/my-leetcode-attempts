0128ee1e0c0768782e7b953327b8be93f61121b0
Kareem Amr Abdelsamie Abdelrahman Younes <official.kyounes@gmail.com>
2024-04-15 15:27:32 +0200

    Solve 153. Find Minimum in Rotated Sorted Array

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.py
@@ -0,0 +1,49 @@
+from math import log
+from turtle import st
+import utils.file_rename as file_rename
+from utils.printers import print_dict  
+
+file_rename.rename_file("153. Find Minimum in Rotated Sorted Array", __file__)
+
+def rotate_array(arr, rotations):
+    # Calculate the actual number of rotations
+    rotations = rotations % len(arr)
+    
+    # Rotate the array using slicing
+    rotated_arr = arr[-rotations:] + arr[:-rotations]
+    
+    return rotated_arr
+
+def solution(array):
+    sz = len(array)
+    start = 0
+    end = sz - 1
+    fail_safe = 0
+
+
+    while (end - start) > 1 and fail_safe < sz:
+        logs = []
+        mid = start + ((end - start) // 2)
+        m = array[mid]
+        s = array[start]
+        e = array[end]
+
+        _min = min(m, s, e)
+
+        if(_min == s): return s
+
+        if(_min == m): end = mid
+        else: start = mid;  
+
+        logs.append(f"{start=} {end=} {arr[start:end + 1]=}")
+
+        fail_safe += 1;
+    
+    return min(array[start:end + 1])
+
+
+r = 10
+for i in range(20):
+    arr = rotate_array([x for x in range(0, 20)], i)
+    print(arr)
+    print(solution(arr))
